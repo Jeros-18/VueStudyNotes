1056b852c328718b82c3577ac034415e8c6d8643
JiaHui <1474352803@qq.com>
2021-08-20 00:38:45 +0800

    单文件组件 分析脚手架

---
+++ vueBasic/19_单文件组件/App.vue
@@ -0,0 +1,21 @@
+<template>
+   <div>
+      <School></School>
+      <Student></Student>
+   </div>
+</template>
+
+<script>
+import School from '../../codes/vue_basic/19_单文件组件/School.vue'
+import Student from '../../codes/vue_basic/19_单文件组件/Student.vue'
+export default {
+  components: { School, Student },
+   name:'App',
+
+
+}
+</script>
+
+<style>
+
+</style>
