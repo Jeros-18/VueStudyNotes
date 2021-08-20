1af9eef613f04f9de373bc43fae2a781018b79cd
JiaHui <1474352803@qq.com>
2021-08-20 18:06:59 +0800

    props配置

--- vueTest/demo/src/App.vue
+++ vueTest/demo/src/App.vue
@@ -1,25 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <School></School>
-    <Student></Student>
+   
+    <h1 v-text='msg' ref="title"></h1></h1>
+    <button ref="btn" @click="showDOM">点我输出上方DOM元素</button>
+    <School ref="sch"></School>
+    
+    
   </div>
 </template>
 
 <script>
 import School from './components/School.vue'
-import Student from './components/Student.vue'
-// import HelloWorld from './components/HelloWorld.vue'
 
 
 export default {
   name: 'App',
   components: {
     School,
-    Student
-    // HelloWorld
-  }
+  },
+  data() {
+    return {
+      msg:'hello',
+    }
+  
+  },
+
+  methods: {
+    showDOM(){
+      console.log(this.$refs.title)
+      console.log(this.$refs.btn)
+      console.log(this.$refs.sch)
+    }
+  },
 }
 </script>
 
