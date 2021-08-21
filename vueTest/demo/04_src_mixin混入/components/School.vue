b3ffc9ecf33e6e518044b6af2b85d29e542ce481
JiaHui <1474352803@qq.com>
2021-08-21 12:39:57 +0800

    mixin混入

---
+++ vueTest/demo/04_src_mixin混入/components/School.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h2 @click="showName">学校名称：{{name}}</h2></h2>
+      <h2>学校地址：{{adress}}</h2>
+  </div>
+</template>
+
+<script>
+ import {hunhe} from '../mixin'
+
+export default {
+   
+    name:'School',
+    data() {
+        return {
+            name:'安徽工程大学',
+            adress:'安徽省芜湖市'
+        }
+    },
+
+    mixins:[hunhe]
+}
+</script>
+
+<style>
+
+</style>
