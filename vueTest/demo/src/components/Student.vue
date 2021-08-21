b423e12b76dd9f430917ae944e64240eb3b1ede0
JiaHui <1474352803@qq.com>
2021-08-21 12:18:18 +0800

    props属性

---
+++ vueTest/demo/src/components/Student.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="demo">
+        <h2>学生姓名：{{name}}</h2>
+        <h2>学生性别：{{sex}}</h2>
+        <h2>学生年龄：{{myAge+1}}</h2>
+        <button @click="updateAge">尝试修改收到的年龄</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data() {
+        return {
+            
+            myAge:this.age
+        }
+    },
+    methods: {
+        updateAge(){
+            this.myAge++
+        }
+    },
+    props:{
+        name:String,
+        age:Number,
+        sex:String
+    }
+}
+</script>
+
+<style>
+    .demo{
+        background-color:blue;
+    }
+</style>
