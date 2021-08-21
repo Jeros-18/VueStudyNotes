7ae60ffc295205dd3ef5132da0dbf3f72356cad3
JiaHui <1474352803@qq.com>
2021-08-21 11:59:11 +0800

    上次提交作废 ref属性

---
+++ vueTest/demo/02_src_ref属性/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+   
+    <h1 v-text='msg' ref="title"></h1></h1>
+    <button ref="btn" @click="showDOM">点我输出上方DOM元素</button>
+    <School ref="sch"></School>
+    
+    
+  </div>
+</template>
+
+<script>
+import School from './components/School.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    School,
+  },
+  data() {
+    return {
+      msg:'hello',
+    }
+  
+  },
+
+  methods: {
+    showDOM(){
+      console.log(this.$refs.title)
+      console.log(this.$refs.btn)
+      console.log(this.$refs.sch)
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
