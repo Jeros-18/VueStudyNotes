3b90246789bf589984eb7c2636832f0256c1b03a
JiaHui <1474352803@qq.com>
2021-09-01 20:34:34 +0800

    todoList 案列

---
+++ vueTest/demo/src/components/MyList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="todo-main">
+    <myItem
+      v-for="todoObj in todos"
+      :key="todoObj.id"
+      :todo="todoObj"
+      :checkTodo="checkTodo"
+      :deleteTodo="deleteTodo"
+    />
+  </ul>
+</template>
+
+<script>
+import myItem from "./myItem";
+export default {
+  name: "MyList",
+  components: { myItem },
+
+  props: ["todos", "checkTodo",'deleteTodo'],
+};
+</script>
+
+<style>
+</style>
